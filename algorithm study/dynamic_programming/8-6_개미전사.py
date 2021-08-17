8fae04e948883ea211d2ae2dda613b48c4944726
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2021-08-17 22:40:03 +0900

    다이나믹 프로그래밍 문제풀이

---
+++ algorithm study/dynamic_programming/8-6_개미전사.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+d = [0] * 100
+d[0] = arr[0]
+d[1] = max(d[0], arr[1])
+
+for i in range(3, n):
+    d[i] = max(d[i - 1], d[i - 2] + arr[i])
+
+print(d[n - 1])
