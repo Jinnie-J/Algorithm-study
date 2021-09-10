6c233cdbba3dcd7eb3543cd9a61385827ddb9de8
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2021-09-10 23:58:42 +0900

    스택 문제풀이

---
+++ python/스택,큐,해쉬,힙/쇠막대기(스택).py
@@ -0,0 +1,17 @@
+s = input()
+
+s = list(s)
+stack = []
+cnt = 0
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(s[i])
+    else:
+        stack.pop()
+        if s[i - 1] == '(':
+            cnt += len(stack)
+        else:
+            cnt += 1
+
+print(cnt)
