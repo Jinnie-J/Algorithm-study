d4861f72ce8b936286c13e8cf355a50e8957c742
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2021-11-18 00:12:00 +0900

    톱니바퀴 풀이 수정

--- baekjoon/[14891]톱니바퀴.py
+++ baekjoon/[14891]톱니바퀴.py
@@ -1,41 +1,44 @@
 from collections import deque
 
-arr = [list(map(int, input())) for _ in range(4)]
-arr= deque(arr)
+arr = []
+for _ in range(4):
+    arr.append(deque(list(map(int, input()))))
+
 k = int(input())
 
 
 def rotate(index, d):
     if d == 1:
-        arr[i].insert(0, arr[i].pop())
-    elif d == -1:
-        arr[i].append(arr[i].popleft())
+        arr[index].insert(0, arr[index].pop())
+    else:
+        arr[index].append(arr[index].popleft())
 
-for i in range(k):
+
+for _ in range(k):
     index, dir = map(int, input().split())
-    rotateArr = [[index-1,dir]]
+    rotateArr = [[index - 1, dir]]
 
-    x= index-1
+    x = index - 1
     nowdir = dir
-    while x+1<=3:
-        if arr[x][2] != arr[x+1][6]:
+    while x + 1 <= 3:
+        if arr[x][2] != arr[x + 1][6]:
             nowdir = -nowdir
-            rotateArr.append([x+1,nowdir])
-        elif arr[x][2] == arr[x+1][6]:
+            rotateArr.append([x + 1, nowdir])
+        else:
             break
-        x+=1
+        x += 1
 
-    x=index-1
-    while x-1>=0:
-        if arr[x][6]!=arr[x-1][2]:
+    x = index - 1
+    nowdir = dir
+    while x - 1 >= 0:
+        if arr[x][6] != arr[x - 1][2]:
             nowdir = -nowdir
-            rotateArr.append([x-1,nowdir])
-        elif arr[x][6] == arr[x-1][2]:
+            rotateArr.append([x - 1, nowdir])
+        else:
             break
-        x-=1
+        x -= 1
 
     for x, dir in rotateArr:
-        rotate(x,dir)
+        rotate(x, dir)
 
-
-print(arr[0][0]*1 + arr[1][0]*2 + arr[2][0]*4 + arr[3][0]*8)
+print(arr[0][0] * 1 + arr[1][0] * 2 + arr[2][0] * 4 + arr[3][0] * 8)
