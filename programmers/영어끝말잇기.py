f533e12dd989c734ff008354e4ee89f62f965cd9
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-03-22 23:50:30 +0900

    코드 수정

--- programmers/영어끝말잇기.py
+++ programmers/영어끝말잇기.py
@@ -1,34 +1,25 @@
-# 정확성 수정필요
 def solution(n, words):
     answer = []
+    whole_word = []
+    p_word = []
 
-    arr = []
-    word_set = []
-    for i in range(n):
-        arr.append([])
+    for _ in range(n):
+        p_word.append([])
 
     i = 0
-    stop = False
-    tmp = words[0][0]
     while i < len(words):
         for j in range(n):
-            if words[i] in word_set or words[i][0] != tmp or len(words[i]) == 1:
-                answer.append(j + 1)
-                answer.append(len(arr[j]) + 1)
-                stop = True
-            else:
-                arr[j].append(words[i])
+            if i > 0:
+                if (words[i] in whole_word) or (words[i][0] != words[i - 1][-1]):
+                    answer.append(j + 1)
+                    answer.append(len(p_word[j]) + 1)
+                    return answer
 
-            word_set.append(words[i])
-            tmp = words[i][len(words[i]) - 1]
+            p_word[j].append(words[i])
+            whole_word.append(words[i])
             i += 1
 
-        if stop == True:
-            break
-    if len(answer) == 0:
-        answer = [0, 0]
-
-    return answer
+    return [0, 0]
 
 
 # 간단한 코드
