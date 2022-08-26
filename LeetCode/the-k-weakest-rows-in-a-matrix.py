020ac0d712313522a0ced2ae6f041de63b4f084d
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-08-27 00:08:04 +0900

    1337. The K Weakest Rows in a Matrix

--- LeetCode/the-k-weakest-rows-in-a-matrix.py
+++ LeetCode/the-k-weakest-rows-in-a-matrix.py
@@ -1,3 +1,4 @@
+# 2차원 배열 풀이
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         arr = []
@@ -7,4 +8,17 @@ class Solution:
         
         answer = [idx[0] for idx in sorted_arr[:k]]
         
+        return answer
+
+# 딕셔너리 풀이
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        dic = {}
+        for i in range(len(mat)):
+            dic[i]= sum(mat[i])
+        sorted_dic = sorted(dic.items(), key=lambda x: x[1])
+        
+        answer=[]
+        for i in range(k):
+            answer.append(sorted_dic[i][0])
         return answer
